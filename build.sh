0e7171ddd1cdcfda83e3662ddbe1b4348aee3028
anu <anu@gmail.com>
2021-03-18 13:52:59 +0530

    jenkins-pipeline-new-update-1.0

--- build.sh
+++
@@ -1,8 +0,0 @@
-call ant -f "%CD%\xyz\build.xml"
-echo ELVL: %ERRORLEVEL% 
-IF NOT %ERRORLEVEL% == 0 ( 
-    echo ABORT: %ERRORLEVEL%
-    call exit /b %ERRORLEVEL%
-) ELSE (
-    echo PROCEED: %ERRORLEVEL%
-)
